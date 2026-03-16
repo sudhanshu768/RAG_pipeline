542509a8819ecc4f177839f6f0b5bbd29c51b446
Sudhanshu Kandekar <kandekar648@gmail.com>
2026-03-16 20:38:54 +0530

    Initial RAG pipeline with ChromaDB and HuggingFace embeddings

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.document_loaders import TextLoader,DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma # chroma can be hosted locally so i am using chroma db 
 from dotenv import load_dotenv
 
@@ -29,7 +29,7 @@ def load_documents(docs_path="docs"):
     documents= loader.load()
 
     if len(documents)==0:
-        raise FileNotFoundError(f"there is no .txt file in {docs_path} . Please add it to your company file")
+        raise FileNotFoundError(f"there is no .txt file in {docs_path} . Please adOpenAIEmbeddingsd it to your company file")
     
     for i, doc in enumerate(documents[:2]):
         print(f"\n Document {i+1}")
@@ -43,7 +43,7 @@ def load_documents(docs_path="docs"):
 def split_documents(documents, chunk_size=1000, chunk_overlap=0):
      """split documents into smaller chunks with overlap""" 
      print("splitting documents into chunk")
-     text_splitter = CharacterTextSplitter(
+     text_splitter = CharacterTextSplitter(         # basic text spliter in langcahne
      chunk_size=chunk_size,
      chunk_overlap=chunk_overlap)
 
@@ -63,6 +63,27 @@ def split_documents(documents, chunk_size=1000, chunk_overlap=0):
      return chunks
 
 
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """create and presist ChromaDB vectore store"""
+    print('creating. embedding and storing in ChromaDB')
+
+    embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+    print("---creating vector store---")
+    vectorestore =Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory, # store locally
+        collection_metadata={"hnsw:space":"cosine"} # algo used to retrive the top result (more on this afterwards)
+    )
+
+    print("---Finished creating vector store---")
+    print(f"vectore store created and saved to {persist_directory}")
+    return vectorestore
+
+
 
 
 #loadign the file
@@ -73,7 +94,9 @@ def main():
 
 #2 chunking the file
     chunks=split_documents(documents)
+
 #3Embedding the sorting the vector DB
+    vectorestore=create_vector_store(chunks)
 
 
 
